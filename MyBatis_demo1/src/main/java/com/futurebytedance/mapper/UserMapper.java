aa62a725c3408fe0b0058c40030969a79d707088
sssv587 <1092663295@qq.com>
2022-09-12 22:40:21 +0800

    MyBatis添加功能

---
+++ MyBatis_demo1/src/main/java/com/futurebytedance/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.futurebytedance.mapper;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/12 - 21:56
+ * @Description MyBatis面向切口编程的两个一致
+ * 1、映射文件的namespace要和mapper接口的全类名保持一致
+ * 2、映射文件中SQL语句要和mapper接口中的方法名一致
+ * <p>
+ * 表-实体类-mapper接口-映射文件
+ */
+public interface UserMapper {
+    /**
+     * 添加用户信息
+     */
+    int insertUser();
+}
---
+++ MyBatis_demo1/src/main/java/com/futurebytedance/test/MyBatisTest.java
@@ -0,0 +1,38 @@
+package com.futurebytedance.test;
+
+import com.futurebytedance.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/12 - 22:24
+ * @Description 测试类
+ */
+public class MyBatisTest {
+    @Test
+    public void testMyBatis() throws IOException {
+        //加载核心配置文件
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        //获取SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        //获取sqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        //获取SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //获取mapper接口对象
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        //测试功能
+        int result = mapper.insertUser();
+        //提交事务
+        sqlSession.commit();
+        System.out.println("result:" + result);
+    }
+}
