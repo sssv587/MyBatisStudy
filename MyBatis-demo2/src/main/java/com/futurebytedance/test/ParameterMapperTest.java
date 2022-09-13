aa76a489bce30f5b85c58cd2b0866e5ebf577d66
sssv587 <1092663295@qq.com>
2022-09-13 16:44:15 +0800

    MyBatis获取参数的两种方式

--- MyBatis-demo2/src/main/java/com/futurebytedance/test/ParameterMapperTest.java
+++ MyBatis-demo2/src/main/java/com/futurebytedance/test/ParameterMapperTest.java
@@ -6,6 +6,9 @@ import com.futurebytedance.utils.SqlSessionUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 /**
@@ -15,11 +18,27 @@ import java.util.List;
  * @Description 测试类
  */
 public class ParameterMapperTest {
+    /**
+     * MyBatis获取参数值得两种方式:${}和#{}
+     * ${}本质字符串拼接
+     * #{}本质占位符赋值
+     */
+
     @Test
     public void testGetAllUser() {
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
         ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
         List<User> list = mapper.getAllUser();
         list.forEach(System.out::println);
+    }
+
+    @Test
+    public void testJDBC() throws Exception {
+        String username = "admin";
+        Class.forName("");
+        Connection connection = DriverManager.getConnection("", "", "");
+        connection.prepareStatement("select * from t_user where username = '" + username + "'");
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from t_user where username=?");
+        preparedStatement.setString(1, username);
     }
 }
