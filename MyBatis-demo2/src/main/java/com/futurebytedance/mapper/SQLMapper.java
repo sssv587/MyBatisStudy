1c98ffd97a04360726bc02ab4b1971c2c938965a
sssv587 <1092663295@qq.com>
2022-09-14 00:19:34 +0800

    模糊查询

---
+++ MyBatis-demo2/src/main/java/com/futurebytedance/mapper/SQLMapper.java
@@ -0,0 +1,19 @@
+package com.futurebytedance.mapper;
+
+import com.futurebytedance.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/14 - 0:10
+ * @Description
+ */
+public interface SQLMapper {
+    /**
+     * 根据用户名模糊查询用户信息
+     */
+    List<User> getUserByLike(@Param("username") String username);
+}
---
+++ MyBatis-demo2/src/main/java/com/futurebytedance/test/SQLMapperTest.java
@@ -0,0 +1,25 @@
+package com.futurebytedance.test;
+
+import com.futurebytedance.mapper.SQLMapper;
+import com.futurebytedance.pojo.User;
+import com.futurebytedance.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/14 - 0:13
+ * @Description
+ */
+public class SQLMapperTest {
+    @Test
+    public void testGetUserByLike() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SQLMapper mapper = sqlSession.getMapper(SQLMapper.class);
+        List<User> list = mapper.getUserByLike("a");
+        System.out.println(list);
+    }
+}
