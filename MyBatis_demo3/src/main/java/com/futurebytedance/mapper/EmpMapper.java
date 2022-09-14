e85e1f9431470da85739c9bdc2ae4ed0c3f840d0
sssv587 <1092663295@qq.com>
2022-09-14 20:17:36 +0800

    搭建框架

---
+++ MyBatis_demo3/src/main/java/com/futurebytedance/mapper/EmpMapper.java
@@ -0,0 +1,10 @@
+package com.futurebytedance.mapper;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/14 - 20:15
+ * @Description
+ */
+public interface EmpMapper {
+}
---
+++ MyBatis_demo3/src/main/java/com/futurebytedance/pojo/Emp.java
@@ -0,0 +1,77 @@
+package com.futurebytedance.pojo;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/14 - 20:13
+ * @Description
+ */
+public class Emp {
+    private Integer eid;
+    private String empName;
+    private Integer age;
+    private String sex;
+    private String email;
+
+    public Emp(Integer eid, String empName, Integer age, String sex, String email) {
+        this.eid = eid;
+        this.empName = empName;
+        this.age = age;
+        this.sex = sex;
+        this.email = email;
+    }
+
+    public Emp() {
+    }
+
+    public Integer getEid() {
+        return eid;
+    }
+
+    public void setEid(Integer eid) {
+        this.eid = eid;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "eid=" + eid +
+                ", empName='" + empName + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
