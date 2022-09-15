3ddcbd0ef0c2b8f4ec651c71d894d7d3d8f1f383
sssv587 <1092663295@qq.com>
2022-09-15 15:58:09 +0800

    动态SQL-where标签

--- MyBatis_demo3/src/test/java/DynamicSQLTest.java
+++ MyBatis_demo3/src/test/java/DynamicSQLTest.java
@@ -17,12 +17,16 @@ public class DynamicSQLTest {
     /**
      * 动态SQL:
      * 1.if:根据标签中test属性所对应的表达式决定标签中的内容是否需要拼接到SQL中
+     * 2.where:
+     * 当where标签中有内容时,会自动生成where关键字,并且将内容前多余的and或or去掉
+     * 当where标签中没有内容时,此时where标签没有任何效果
+     * 注意:where标签不能将其中内容后面多余的and或or去掉
      */
     @Test
     public void testGetEmpByCondition() {
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
         DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
-        List<Emp> list = mapper.getEmpByCondition(new Emp(null, "", null, "男", "123@qq.com"));
+        List<Emp> list = mapper.getEmpByCondition(new Emp(null, "张三", null, "", ""));
         System.out.println(list);
     }
 }
